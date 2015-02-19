75e00309219434c5ac3510c9562fafe4a980a70e
JadeMatrix <jadematrix.art@gmail.com>
2015-02-19 13:50:40 -0500

    Added collective and namespace alias headers

---
+++ src/jadebase.hpp
@@ -0,0 +1,65 @@
+#ifndef JADEBASE_HPP
+#define JADEBASE_HPP
+
+/* 
+ * jadebase.hpp
+ * 
+ * Collective header for jadebase
+ * 
+ */
+
+/* INCLUDES *******************************************************************//******************************************************************************/
+
+#include "filetypes/jb_png.hpp"
+
+#include "gui/jb_button.hpp"
+// #include "gui/jb_canvasview.hpp"
+#include "gui/jb_dial.hpp"
+#include "gui/jb_element.hpp"
+#include "gui/jb_group.hpp"
+#include "gui/jb_image_rsrc.hpp"
+#include "gui/jb_named_resources.hpp"
+#include "gui/jb_resource.hpp"
+#include "gui/jb_resource_names.hpp"
+#include "gui/jb_scrollable.hpp"
+#include "gui/jb_scrollset.hpp"
+#include "gui/jb_tabset.hpp"
+#include "gui/jb_text_rsrc.hpp"
+#include "gui/jb_texture.hpp"
+
+#include "main/jb_main.h"
+
+#include "tasking/jb_task.hpp"
+#include "tasking/jb_taskexec.hpp"
+// #include "tasking/jb_taskqueue.hpp"
+#include "tasking/jb_taskutil.hpp"
+
+#include "threading/jb_condition.hpp"
+#include "threading/jb_mutex.hpp"
+#include "threading/jb_scopedlock.hpp"
+#include "threading/jb_semaphore.hpp"
+#include "threading/jb_thread.hpp"
+#include "threading/jb_threadutil.hpp"
+
+#include "utility/jb_exception.hpp"
+#include "utility/jb_gl.hpp"
+#include "utility/jb_launchargs.hpp"
+#include "utility/jb_log.hpp"
+#include "utility/jb_platform.h"
+#include "utility/jb_settings.hpp"
+#include "utility/jb_timestamp.hpp"
+#include "utility/jb_exception.hpp"
+// #include "utility/jb_trackable.hpp"
+#include "utility/jb_version.hpp"
+
+#include "windowsys/jb_events.hpp"
+#include "windowsys/jb_keycode.hpp"
+#include "windowsys/jb_window.hpp"
+#include "windowsys/jb_windowevent.hpp"
+#include "windowsys/jb_windowmanagement.hpp"
+
+/******************************************************************************//******************************************************************************/
+
+#endif
+
+
