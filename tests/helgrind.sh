cdd6bc0315b1b478981d392046678c236a4c7fcd
JadeMatrix <jadematrix.art@gmail.com>
2015-11-03 20:53:46 -0500

    Tweaked/updated Valgrind tests

---
+++ tests/helgrind.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+valgrind --tool=helgrind --demangle=no --num-callers=50 --free-is-write=yes --suppressions=tests/suppressions/linux.suppressions make/build/jb_test -d -G "resources/cfg/defaults_general.cfg" -G "resources/cfg/defaults_linux.cfg" -s "src/jb_test.lua" -g "local/test.cfg" --gui-scale 2
+
